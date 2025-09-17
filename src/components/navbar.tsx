b98d170b935ea4bebdbdfa2990a16d192b459c38
Akaash-Walker <awalker7179@gmail.com>
2025-09-17 15:41:21 -0400

    fix navbar label

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <nav className="navbar bg-base-300 px-4">
             <div className="navbar-start">
-                <a className="btn btn-ghost text-xl">My CRUD App</a>
+                <a className="label text-xl text-white font-bold">My CRUD App</a>
             </div>
             <div className="navbar-end">
                 <button className="btn btn-primary">Login</button>
