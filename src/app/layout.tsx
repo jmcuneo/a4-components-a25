cc07fcba4cc1ab6ceb01e17f2d5edb225fcd9ee7
Akaash-Walker <awalker7179@gmail.com>
2025-09-21 00:02:23 -0400

    add adobe font (avenir)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import "./globals.css";
-import Navbar from "@/components/navbar";
+import React from 'react';
+import './globals.css';
+import Navbar from '@/components/navbar';
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout(
+    {children}: { children: React.ReactNode }
+) {
     return (
         <html lang="en">
+        <head>
+            <title>My CRUD APP</title>
+            <link rel="preconnect" href="https://use.typekit.net" crossOrigin="anonymous"/>
+            <link rel="stylesheet" href="https://use.typekit.net/kxi4uok.css"/>
+        </head>
         <body>
         <Navbar/>
         {children}
