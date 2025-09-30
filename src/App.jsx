5b73774241fa0c3b68aa5be9fcc6b33638880ba0
christopheryon <203912328+christopheryon@users.noreply.github.com>
2025-09-29 21:48:12 -0400

    Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,81 @@
-import { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
 import 'beercss'
-import React from "react";
 import NewButton from "./NewButton.jsx";
 
-const response =await fetch("/passwords", {
-    method: "GET"
-})
-
-const passwords = JSON.parse(await response.text())
 
 function App() {
+    const [passwords, setPasswords] = useState([{
+        website: "Loading",
+        username: "Loading",
+        password: "Loading",
+        strength: "Loading"
+    }])
+    const getPasswords = async () => {
+        const response = await fetch("/passwords", {
+            method: "GET"
+        })
+        setPasswords(JSON.parse(await response.text()))
+    }
 
-  return (
-    <>
-        {passwords.map((entry) =>
-            (<tr>
-                <td>
-                    {entry.website}
-                </td>
-                <td>
-                    {entry.username}
-                </td>
-                <td>
-                    {entry.password}
-                </td>
-                <td>
-                    {entry.strength}
-                </td>
-            </tr>)
-        )}
-        <NewButton></NewButton>
-    </>
-  )
+    useEffect(async () => {
+        await getPasswords()
+    }, []);
+    return (
+        <>
+            <div className={"mainContainer"}>
+
+
+                <div id="header">
+                    <h2>
+                        My Passwords
+                    </h2>
+                </div>
+                <div id={"passwordTableContainer"}>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>
+                                Website
+                            </th>
+                            <th>
+                                Username
+                            </th>
+                            <th>
+                                Password
+                            </th>
+                            <th>
+                                Strength
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {passwords.map((entry) =>
+                            (<tr>
+                                <td>
+                                    {entry.website}
+                                </td>
+                                <td>
+                                    {entry.username}
+                                </td>
+                                <td>
+                                    {entry.password}
+                                </td>
+                                <td>
+                                    {entry.strength}
+                                </td>
+                            </tr>)
+                        )}
+                        </tbody>
+                    </table>
+
+
+                </div>
+
+            </div>
+            <NewButton></NewButton>
+        </>
+    )
 }
 
 export default App
