475781708c2cf7cfff6a07603c3865337e40b8a2
christopheryon <203912328+christopheryon@users.noreply.github.com>
2025-09-30 17:34:10 -0400

    Account banner and test row buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
-import './App.css'
-import 'beercss'
 import NewButton from "./NewButton.jsx";
+import 'beercss'
+import './App.css'
 
 
 function App() {
@@ -11,21 +11,50 @@ function App() {
         password: "Loading",
         strength: "Loading"
     }])
+    const [user, setUser] = useState({
+        avatar_url: "",
+        username: "User"
+    })
     const getPasswords = async () => {
         const response = await fetch("/passwords", {
             method: "GET"
         })
         setPasswords(JSON.parse(await response.text()))
     }
-
+    const getUsername = async () => {
+        const response = await fetch("/user", {
+            method: "GET"
+        })
+        const userString = await response.text()
+        setUser(JSON.parse(userString))
+    }
     useEffect(async () => {
         await getPasswords()
+        await getUsername()
     }, []);
     return (
         <>
+            <body className={"dark"}>
             <div className={"mainContainer"}>
 
+                <div className={"accountContainer bottom-margin"}>
+                    <span>
+                        {
+                            user.avatar_url !== ""
+                                ? <img src={user.avatar_url} className={"circle tiny small-margin right-margin"} alt={"avatar image"}/>
+                                : <i className={"circle extra small-margin right-margin"}>account_circle</i>
+                        }
 
+                        <span className={"large-text"}>{user.username}</span>
+                    </span>
+                    <button className={"small circle absolute right"} onClick={() => {
+                        window.location.href = "/logout"
+                    }}>
+                        <i>
+                            logout
+                        </i>
+                    </button>
+                </div>
                 <div id="header">
                     <h2>
                         My Passwords
@@ -50,8 +79,9 @@ function App() {
                         </tr>
                         </thead>
                         <tbody>
-                        {passwords.map((entry) =>
-                            (<tr>
+                        {passwords.map((entry, index) =>
+                            (
+                                (<tr>
                                 <td>
                                     {entry.website}
                                 </td>
@@ -64,7 +94,11 @@ function App() {
                                 <td>
                                     {entry.strength}
                                 </td>
+                                    {
+                                        true ? (<td><button className={"edit-save-button transparent circle"}><i>save</i></button></td>) : (<td>bye</td>)
+                                    }
                             </tr>)
+                            )
                         )}
                         </tbody>
                     </table>
@@ -74,6 +108,7 @@ function App() {
 
             </div>
             <NewButton></NewButton>
+            </body>
         </>
     )
 }
