3680708779db20ded3e7c00b3687c6ace83191f9
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-09-30 20:35:48 -0400

    changed callback url

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ passport.deserializeUser((obj, done) => {
 passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        // callbackURL: "https://a3-kayliequach.vercel.app/auth/github/callback"
-        callbackURL: "http://localhost:3000/auth/github/callback"
+        callbackURL: "https://a4-kayliequach.vercel.app/auth/github/callback"
+        // callbackURL: "http://localhost:3000/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, cb) {
         console.log("GitHub profile:", profile)
