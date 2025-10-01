0c45340c3b2ed523f7b41a4aa6ddf6b03a5b60b5
ElijahGray99 <123423522+ElijahGray99@users.noreply.github.com>
2025-10-01 17:42:54 -0400

    Tried to make my website more modular. The previous version was working just fine. I hope this works on render! Here goes nothing!

--- my-react-app/src/components/User_content.jsx
+++ my-react-app/src/components/User_content.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
+import Homework_table from "./Homework_table";
 
 
 
@@ -283,37 +284,7 @@ export default function User_content() {
 
                 </div>
 
-                <div className="card p-4 mb-4">
-                    <h2 className="h5 mb-3">Your Assignments</h2>
-                    <div className="table-responsive-xl shadow-lg">
-
-                        <table className="table table-striped table-hover" id="homework-table">
-
-                            <thead>
-                                <tr className="table-primary text-uppercase">
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Subject</th>
-                                    <th scope="col">Hours Expected</th>
-                                    <th scope="col">Due Date</th>
-                                    <th scope="col">Stress Score</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-
-                                {homework.map(homework => (
-                                    <tr key = {homework.ID}>
-                                            <td>{homework.ID}</td> <td>{homework.subject}</td> <td>{homework.expectedtime}</td>
-                                            <td>{new Date(homework.date).toLocaleDateString()}</td>
-                                            <td>{homework.stress_score}</td>
-                                    </tr>)
-                                )}
-
-                            </tbody>
-
-                        </table>
-                    </div>
-                </div>
+                <Homework_table homework = {homework} />
 
             </main>
         </>
