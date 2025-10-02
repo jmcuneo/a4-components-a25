37e2afd2bfcf006f3fda0c53c58210f75b6717a0
BigMouthInc <144144777+BigMouthInc@users.noreply.github.com>
2025-10-01 22:18:39 -0400

    forgot to remove proxy server

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy:{
-      "/login": "http://localhost:3000",
-      "/table": "http://localhost:3000",
-      "/fields": "http://localhost:3000",
-      "/add": "http://localhost:3000",
-      "/remove": "http://localhost:3000",
-      "/update": "http://localhost:3000",
-      "/docs": "http://localhost:3000",
-    }
-  }
 });
