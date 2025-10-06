6fe4f68bab54ed09053d614fcc05fb3921020117
mtnapoleone <mnapoleone@wpi.edu>
2025-10-05 20:15:15 -0400

    updating server.js for render

--- server.js
+++ server.js
@@ -12,17 +12,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const distPath = path.join(__dirname, "dist");
-app.use(express.static(distPath));
 
-app.use((req, res, next) => {
-    if (req.method !== "GET") return next();
-    const accept = req.headers.accept || "";
-    if (!accept.includes("text/html")) return next();
-    res.sendFile(path.join(distPath, "index.html"), (err) => {
-        if (err) next(err);
-    });
+app.use(express.static(distPath, {
+    setHeaders: (res, filePath) => {
+        if (path.extname(filePath) === ".js") {
+            res.setHeader("Content-Type", "application/javascript");
+        }
+    },
+}));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(` Server running on port ${PORT}`);
 });
